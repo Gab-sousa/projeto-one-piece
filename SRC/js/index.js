e99ea885731150466d6db010c6aa56bf36dc599a
Gabriel Mendes de Sousa <141192631+Gab-sousa@users.noreply.github.com>
2023-10-03 22:46:29 -0300

    Add files via upload

--- SRC/js/index.js
+++ SRC/js/index.js
@@ -1,27 +1,36 @@
 const botoes = document.querySelectorAll('.botao');
-const personagens = document.querySelectorAll('.personagem'); 
+const personagens = document.querySelectorAll('.personagem');
+const logos = document.querySelectorAll('.logo');
 
 botoes.forEach((botao, indice) => {
     botao.addEventListener("click", () => {
-
         desselecionarBotao();
         desselecionarPersonagem();
 
         botao.classList.add("selecionado");
         personagens[indice].classList.add("selecionado");
+        document.querySelector('.personagens').classList.add("after")
+
+    });
+});
+
+logos.forEach((logo) => {
+    logo.addEventListener("click", () => {
+        document.querySelector('.personagens').classList.remove("after")
+        desselecionarBotao();
+        desselecionarPersonagem();
+
+        document.querySelector('.home').classList.add("selecionado");
 
     });
 });
 
 function desselecionarBotao() {
-    const botaoSelecionado = document.querySelector(".botao.selecionado");
-    botaoSelecionado.classList.remove("selecionado");
+    const botoesSelecionados = document.querySelectorAll(".botao.selecionado");
+    botoesSelecionados.forEach(botao => botao.classList.remove("selecionado"));
 };
 function desselecionarPersonagem() {
-    const personagemSelecionado = document.querySelector(".personagem.selecionado");
-    personagemSelecionado.classList.remove("selecionado");
+    const personagensSelecionados = document.querySelectorAll(".personagem.selecionado");
+    personagensSelecionados.forEach(personagem => personagem.classList.remove("selecionado"));
+
 };
-
-
-
-
